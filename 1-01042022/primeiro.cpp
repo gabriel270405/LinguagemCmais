795928f5019593beb95eec97f1188b76ddd84494
Gabriel <101403161+gabriel270405@users.noreply.github.com>
2022-04-04 08:46:22 -0300

    Add files via upload

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,16 @@
+/*
+Programa pra exibir uma mensagem. O código foi escrito
+em c++
+*/
+/*
+Importação da biblioteca de entrada e saida de dados
+*/
+#include <iostream>
+
+int main() {
+
+    //saida padrão de dados 
+    std::cout << "bem Vindo ao c++\n";
+
+    return 0;
+}
